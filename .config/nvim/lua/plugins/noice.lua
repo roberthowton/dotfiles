f6a3e27c2bf2b9ced5c9b55ed30822b53d7cff1b
robert howton <robert.f.howton@gmail.com>
2025-08-13 14:46:37 -0400

    fix "no information available" notification spam

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,20 @@
+-- lazy.nvim
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      routes = {
+        {
+          filter = {
+            event = "notify",
+            find = "No information available",
+          },
+          opts = {
+            skip = true,
+          },
+        },
+      },
+    },
+  },
+}
